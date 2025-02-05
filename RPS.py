76ef62a3da266d38b4caaa0712f600a8801c617f
eehernandez14 <eehernandez@unomaha.edu>
2025-02-05 15:59:22 +0000

    Done

--- RPS.py
+++ RPS.py
@@ -10,28 +10,32 @@ def main():
   losses = 0
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
+  
+  play = "Y"
+  while play == "Y":
+    #Randomly choose the computer between 'R', 'P', or 'S'
+    #Prompt the user for their RPS selection
+    #Determine winner and state what happened to the user
+    #Ask the user if they would like to play again.
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
+    player = input("Enter choise (R/P/S): ")
 
-  player = input("Enter choise (R/P/S): ")
+    computer = random.choice( ["R", "P", "S"] )
 
-  computer = random.choice( ["R", "P", "S"] )
-
-  if player == "R" and computer == "R":
-    print("You tie")
-    ties = ties + 1
-  elif player == "R" and computer == "P":
-    print("You Losses")
-    losses = losses + 1 
-  elif player == "R" and computer == "S":
-    print("You Win")
-    wins = wins + 1
-  else:
-    print("That is Invalid Choise")
-    print("try again")
+    if player == "R" and computer == "R":
+      print("You tie")
+      ties = ties + 1
+    elif player == "R" and computer == "P":
+      print("You Losses")
+      losses = losses + 1 
+    elif player == "R" and computer == "S":
+      print("You Win")
+      wins = wins + 1
+    else:
+      print("That is Invalid Choise")
+      print("try again")
+  
+    play = input("Play again (Y/N): ")
 
   #In the end, print the stats
   print("Wins \t Ties \t Losses")
